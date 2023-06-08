971502f7a36dcc81096d40e2fe833b1b90e258d9
Andrew Roan <roanutil@users.noreply.github.com>
2023-06-08 12:38:29 -0500

    v23.4.0

---
+++ package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.8
+
+import PackageDescription
+
+let package = Package(
+name: "citrix-mam-sdk",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(name: "CitrixLogger", targets: ["CitrixLogger"]),
+        .library(name: "CTXMAMAppCore", targets: ["CTXMAMAppCore"]),
+        .library(name: "CTXMAMCompliance", targets: ["CTXMAMCompliance"]),
+        .library(name: "CTXMAMContainment", targets: ["CTXMAMContainment"]),
+        .library(name: "CTXMAMCore", targets: ["CTXMAMCore"]),
+        .library(name: "CTXMAMLocalAuth", targets: ["CTXMAMLocalAuth"]),
+        .library(name: "CTXMAMNetwork", targets: ["CTXMAMNetwork"]),
+    ],
+    targets: [
+        .binaryTarget(name: "CitrixLogger", path: "./Sources/CitrixLogger.xcframework"),
+        .binaryTarget(name: "CTXMAMAppCore", path: "./Sources/CTXMAMAppCore.xcframework"),
+        .binaryTarget(name: "CTXMAMCompliance", path: "./Sources/CTXMAMCompliance.xcframework"),
+        .binaryTarget(name: "CTXMAMContainment", path: "./Sources/CTXMAMContainment.xcframework"),
+        .binaryTarget(name: "CTXMAMCore", path: "./Sources/CTXMAMCore.xcframework"),
+        .binaryTarget(name: "CTXMAMLocalAuth", path: "./Sources/CTXMAMLocalAuth.xcframework"),
+        .binaryTarget(name: "CTXMAMNetwork", path: "./Sources/CTXMAMNetwork.xcframework"),
+    ]
+)
