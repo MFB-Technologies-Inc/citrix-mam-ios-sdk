b82b752a60076d2e1a186b8e5e904b2f82b9a951
Andrew Roan <roanutil@users.noreply.github.com>
2025-06-23 17:06:37 -0500

    Change to remote frameworks and update to 25.5.0
    feature/v25.5.0

--- Package.swift
+++ Package.swift
@@ -22,12 +22,40 @@ let package = Package(
         .library(name: "CTXMAMNetwork", targets: ["CTXMAMNetwork"]),
     ],
     targets: [
-        .binaryTarget(name: "CitrixLogger", path: "./Sources/CitrixLogger.xcframework"),
-        .binaryTarget(name: "CTXMAMAppCore", path: "./Sources/CTXMAMAppCore.xcframework"),
-        .binaryTarget(name: "CTXMAMCompliance", path: "./Sources/CTXMAMCompliance.xcframework"),
-        .binaryTarget(name: "CTXMAMContainment", path: "./Sources/CTXMAMContainment.xcframework"),
-        .binaryTarget(name: "CTXMAMCore", path: "./Sources/CTXMAMCore.xcframework"),
-        .binaryTarget(name: "CTXMAMLocalAuth", path: "./Sources/CTXMAMLocalAuth.xcframework"),
-        .binaryTarget(name: "CTXMAMNetwork", path: "./Sources/CTXMAMNetwork.xcframework"),
+        .binaryTarget(
+            name: "CitrixLogger",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CitrixLogger.xcframework.zip",
+            checksum: "87d0822403521355115b3dfada5ba3735c270cd0cebb68d337f927884479aadc"
+        ),
+        .binaryTarget(
+            name: "CTXMAMAppCore",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMAppCore.xcframework.zip",
+            checksum: "5a7922696b28db81417fed4f861ff4c2468cb0602149f9dd80d87ddbb21a2dc7"
+        ),
+        .binaryTarget(
+            name: "CTXMAMCompliance",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMCompliance.xcframework.zip",
+            checksum: "a08f6e3f0e2d339c4a76e356b320b93a1a8f4a5475e9744893e4fac72a86bd44"
+        ),
+        .binaryTarget(
+            name: "CTXMAMContainment",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMContainment.xcframework.zip",
+            checksum: "58883d9aaae499cc07c6da85df0f61ff12bea71f00e1229226327f38a716e7ec"
+        ),
+        .binaryTarget(
+            name: "CTXMAMCore",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMCore.xcframework.zip",
+            checksum: "c3cdcd2dc664e0f8df8f03090f60a09be5a9143c3ed02ea8fbf84cd1db780909"
+        ),
+        .binaryTarget(
+            name: "CTXMAMLocalAuth",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMLocalAuth.xcframework.zip",
+            checksum: "11b20e5446bb04947f452f094b11c6576590f5b784aa21707b894074700d61d1"
+        ),
+        .binaryTarget(
+            name: "CTXMAMNetwork",
+            url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.5.0/CTXMAMNetwork.xcframework.zip",
+            checksum: "a3b1d3432b0bebb635a21e8d09a8b1d89fc43d9e2669b146dd47c32f18b24c83"
+        ),
     ]
 )
