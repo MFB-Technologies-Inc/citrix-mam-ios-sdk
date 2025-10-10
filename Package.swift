c2878b30f5ec77b5847557b28013110d8f748b0b
Ryan Jarvis <ryan@Ryans-Mac-mini.local>
2025-10-10 15:57:17 -0500

    Fix checksums

--- Package.swift
+++ Package.swift
@@ -25,37 +25,37 @@ let package = Package(
         .binaryTarget(
             name: "CitrixLogger",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CitrixLogger.xcframework.zip",
-            checksum: "43d82946fb52bb84de4bef4e1a85cceac13da68538bccfbcc19b6d93622616e8"
+            checksum: "2e0d905ca9a81bc9a08be68f563395adc57ecf0a54e536130a3aef69bb8ad258"
         ),
         .binaryTarget(
             name: "CTXMAMAppCore",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMAppCore.xcframework.zip",
-            checksum: "4a3d867a9582f7c9b9acf74ed9af5d891b534ce513ea8840b1ca0d40d5174e02"
+            checksum: "aebb945de881dfc4d1d14c92e027af1b909d03d02bb0ef0ed2a7ac411e4d3b03"
         ),
         .binaryTarget(
             name: "CTXMAMCompliance",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMCompliance.xcframework.zip",
-            checksum: "3a89d91bcbddd39a8e82dde3d4fdb1e9b6c03dc0680971bb7611bb8b020e2fa1"
+            checksum: "07e55e6f9f24c8cae82be007908c4248387b53c80b806aae8134f515fa1fd9ad"
         ),
         .binaryTarget(
             name: "CTXMAMContainment",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMContainment.xcframework.zip",
-            checksum: "4de76944e0280c1057713995a347c1436ec4943abf43608e8223215f884821c4"
+            checksum: "976a8f6ae3a9983cdb634e3126df47bc94155fbb99f75e113f8eadadcf370a7e"
         ),
         .binaryTarget(
             name: "CTXMAMCore",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMCore.xcframework.zip",
-            checksum: "ea5c2bd4ce37cd701515cfbbea1763c59951ad12af16e41cef852c7f0a8f6114"
+            checksum: "2fb1708a507d9fd36d5afb3abb94a47a214e3f70c42319d38ca948ffdfabfdfb"
         ),
         .binaryTarget(
             name: "CTXMAMLocalAuth",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMLocalAuth.xcframework.zip",
-            checksum: "2b6a520e70605a1bc361e9031e22de781d6dfdbdeccbcf6164525d9c1c1725b4"
+            checksum: "b4af54356db92d875c3908c5928ab07930848312a4903559d27a2e77c3b3830d"
         ),
         .binaryTarget(
             name: "CTXMAMNetwork",
             url: "https://github.com/MFB-Technologies-Inc/citrix-mam-ios-sdk/releases/download/pre-25.9.0/CTXMAMNetwork.xcframework.zip",
-            checksum: "42eb7d0a5e34da25dd8f72c3c52a528f410a288c747e45248535ee57fd3887a8"
+            checksum: "7241407322ebeeefa3f18c26e10a14d0ae973129504547a69959ab82967b8e21"
         ),
     ]
 )
